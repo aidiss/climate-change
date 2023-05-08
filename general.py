bc4db3a6da0be351baa0d39028a5eb6bd1f99a0d
valdasjurk <valdas.jurk@gmail.com>
2023-05-08 23:16:04 +0300

    add dataset link to constant fix #1

--- general.py
+++ general.py
@@ -1,9 +1,11 @@
 import kaggle
 
+DATASET_LINK = "berkeleyearth/climate-change-earth-surface-temperature-data"
+
 
 def download_dataset() -> None:
     kaggle.api.dataset_download_files(
-        dataset="berkeleyearth/climate-change-earth-surface-temperature-data",
+        dataset=DATASET_LINK,
         path="data",
         unzip=True,
         quiet=False,
